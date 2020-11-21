06bb74ea7aec72923aef6e1c4824e6338f95739d
lolyparty <ololadeolayiwola01@gmail.com>
2020-11-21 13:01:00 +0100

    removed ionic icons preventing caching of files

--- sw.js
+++ sw.js
@@ -1,49 +1,39 @@
 //cahe assets;
-const cacheName = 'cache-v4';
+const cacheName = 'cache-v1';
 const cachedAssets = [
     '/',
     '/index.html',
     'style.css',
     'app.js',
-    'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
-    'ionicons/2.0.1/css/ionicons.min.css',
-    'icons/budgetit-48.png',
-    'icons/budgetit-72.png',
-    'icons/budgetit-96.png',
-    'icons/budgetit-120.png',
-    'icons/budgetit-144.png',
-    'icons/budgetit-152.png',
-    'icons/budgetit-167.png',
-    'icons/budgetit-180.png',
-    'icons/budgetit-192.png',
-    'icons/budgetit-512.png',
     'img/budget.jpeg'
 ]
 
-self.addEventListener('install',(e)=>{
-    e.waitUntil(caches.open(cacheName)
-    .then((cache)=>{
-        cache.addAll(cachedAssets)
-    })
-    .catch(error=>error)
+//install event
+self.addEventListener('install',(event)=>{
+    event.waitUntil(caches.open(cacheName).then(cache=>{
+         cache.addAll(cachedAssets)
+    }))
+})
+
+
+
+// activate event
+self.addEventListener('activate',evt=>{
+    evt.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(keys
+                .filter(key => key !== cacheName)
+                .map(key=>caches.delete(key)))
+        })
     )
 })
 
-self.addEventListener('activate',(e)=>{
-    e.waitUntil(caches.keys().then(keys=>{
-        return Promise.all(keys
-            .filter(key=>key !== cacheName)
-            .map(key=>caches.delete(key)));
-    })
-    )}
-)
-
-self.addEventListener('fetch',(e)=>{
-    e.respondWith(
-        caches.match(e.request)
+self.addEventListener('fetch',evt=>{
+    console.log('fetch request successful')
+    evt.respondWith(
+        caches.match(evt.request)
         .then(cacheRes=>{
-            return cacheRes || fetch(e.request)
+            return cacheRes || fetch(evt.request)
         })
-        .catch(error=>error)
     )
 })
